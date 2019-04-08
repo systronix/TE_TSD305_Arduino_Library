40a5a3cb541922e1d4091dafcb87c95b15b06ee1
systronix <bboyes@systronix.com>
2019-04-08 17:37:33 -0600

    cleanup and formatting of output

--- examples/tsd305_demo/tsd305_demo.ino
+++ examples/tsd305_demo/tsd305_demo.ino
@@ -3,8 +3,11 @@ Adapting the TSD305 demo program to work on the TE Weathershield
 
 I2C lines go through a MUX on the Weathershield so a given sensor has to be 'selected' via the MUX.
 
-Revisions
-2019Apr03 bboyes  start
+----- Revisions -----
+2019Apr03 bboyes  start, based on TE example
+
+----- TODO -----
+Add better time detection so time output is always a multiple of the desired interval
 
 */
 
@@ -19,9 +22,8 @@ void setup() {
   // Wait here for 10 seconds to see if we will use Serial Monitor, so output is not lost
   while((!Serial) && (millis()<10000));    // wait until serial monitor is open or timeout
 
-  Serial.println("==== TE Connectivity ====");
-  Serial.println("======== TSD305 =========");
-  Serial.println("======== Measure ========");
+  Serial.println("==== TE Connectivity TSD305 ====");
+  Serial.printf("Seconds, Sensor\xC2\xB0\x43, Object\xC2\xB0\x43\n");
 
   // WeatherShield MUX control pins
   pinMode(9, OUTPUT);
@@ -36,32 +38,33 @@ void setup() {
   m_tsd305.begin();
 }
 
-void loop() {
+void loop() 
+{
   tsd305_status status;
   float temperature;
   float object_temperature;
   boolean connected;
 
   connected = m_tsd305.is_connected();
-  if (connected) {
-    Serial.println(connected ? "Sensor Connected" : "Sensor Disconnected");
-
+  if (connected) 
+  {
     status = m_tsd305.read_temperature_and_object_temperature(
         &temperature, &object_temperature);
 
-    Serial.print("---Temperature = ");
-    Serial.print(temperature, 1);
-    // Serial.print((char)176); // should be degree symbol but doesn't work in TyCommander serial window
-    Serial.println("C");
+    // Serial.printf("Sensor Temp= %3.1f \xC2\xB0\x43, ", temperature);   // UTF-8 degree symbol and C
+    // Serial.printf("Object Temp= %3.1f \xC2\xB0\x43\n", object_temperature);   // UTF-8 degree symbol and C
 
-    Serial.print("---Object Temperature = ");
-    Serial.print(object_temperature, 1);
-    // Serial.print((char)176);
-    Serial.println("C");
-    Serial.println();
-  } else {
-    Serial.println(connected ? "Sensor Connected" : "Sensor Disconnected");
+    Serial.printf("%4u,  %3.1f,  %3.1f\n", millis()/1000, temperature, object_temperature);   // for CSV file use
+
+  } 
+  else 
+  {
+    Serial.println("Sensor Disconnected");
   }
 
-  delay(1000);
+  delay(5000);
 }
+
+    // Serial.printf("Temp= %4.2f \xC2\xB0\x43,", temperature);   // UTF-8 degree symbol and C
+    // Serial.printf(" Press= %5.2f hPa,", pressure);
+    // Serial.printf(" Hum= %4.2f %%RH\n", humidity);
